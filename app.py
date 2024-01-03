d02d51621e2e2b7d05b22065253dfa7982bc481d
“Aniket <aniket99132@gmail.com>
2024-01-03 15:49:47 +0530

    commited new story developer A

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 def addition (x,y,z): 
     print (x+y+z)
-=======
 ## addition 
 def addition (x,y,z): 
->>>>>>> 0f977da58b7075f34e2aa5ba3c2a7a047fbfce53
     return x+y+z
 
 def multiplicatoion (x,y) :
     return x*y 
+
+def division (x,y):
+    return x/y 
